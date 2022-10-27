e893c6bd84b17354eccc8e521746203868eeb39b
Steven <stevengf14@gmail.com>
2022-10-26 22:24:10 -0500

    First commit - upload config files and base project

---
+++ src/test/java/ec/learning/springboot/app/products/SpringbootServiceProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.learning.springboot.app.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
