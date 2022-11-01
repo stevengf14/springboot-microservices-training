d228647375fe357a99626a0f6018bf07b402171d
Steven Guamán <stevengf14@gmail.com>
2022-10-31 22:33:33 -0500

    Delete src directory

--- src/main/java/ec/learning/springboot/app/products/models/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package ec.learning.springboot.app.products.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ec.learning.springboot.app.products.models.entity.Product;
-
-public interface ProductDao extends CrudRepository<Product, Long> {
-
-}
