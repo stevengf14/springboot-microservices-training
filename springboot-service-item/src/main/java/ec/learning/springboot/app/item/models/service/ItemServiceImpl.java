287df4759608cfdca13a16dd3da8207c2b68d841
Steven <stevengf14@gmail.com>
2022-11-01 23:11:41 -0500

    service with restTemplate

--- springboot-service-item/src/main/java/ec/learning/springboot/app/item/models/service/ItemServiceImpl.java
+++ springboot-service-item/src/main/java/ec/learning/springboot/app/item/models/service/ItemServiceImpl.java
@@ -1,12 +1,17 @@
 package ec.learning.springboot.app.item.models.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import ec.learning.springboot.app.item.models.Item;
+import ec.learning.springboot.app.item.models.Product;
 
 /**
  *
@@ -17,14 +22,19 @@ public class ItemServiceImpl implements IItemService {
 
 	@Autowired
 	private RestTemplate restClient;
-	
+
 	@Override
 	public List<Item> findAll() {
-		return null;
+		List<Product> products = Arrays
+				.asList(restClient.getForObject("http://localhost:8001/getAll", Product[].class));
+		return products.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
 	@Override
 	public Item findById(Long id, Integer quantity) {
+		Map<String, String> pathVariables = new HashMap<>();
+		pathVariables.put("id", id.toString());
+		Product product = restClient.getForObject("http://localhost:8001/get/{id}", Product.class, pathVariables);
 		return null;
 	}
 
