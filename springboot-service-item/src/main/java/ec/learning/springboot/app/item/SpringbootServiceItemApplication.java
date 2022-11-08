a06523bc2ee91b547423130db12aa421f6e671a9
Steven <stevengf14@gmail.com>
2022-11-07 21:50:16 -0500

    Connecting item and products services as Eureka clients

--- springboot-service-item/src/main/java/ec/learning/springboot/app/item/SpringbootServiceItemApplication.java
+++ springboot-service-item/src/main/java/ec/learning/springboot/app/item/SpringbootServiceItemApplication.java
@@ -2,14 +2,14 @@ package ec.learning.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  *
  * @author Steven Guamán - November 2022
  */
-@RibbonClient(name = "service-products")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServiceItemApplication {
--- springboot-service-products/src/main/java/ec/learning/springboot/app/products/SpringbootServiceProductsApplication.java
+++ springboot-service-products/src/main/java/ec/learning/springboot/app/products/SpringbootServiceProductsApplication.java
@@ -2,11 +2,13 @@ package ec.learning.springboot.app.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
 *
 * @author Steven Guamán - October 2022
 */
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServiceProductsApplication {
 
