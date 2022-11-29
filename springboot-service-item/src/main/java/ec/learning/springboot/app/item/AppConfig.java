711dac8df68891ee4e7dd67a43f03f94fa53e9f9
Steven <stevengf14@gmail.com>
2022-11-28 21:56:21 -0500

    SlowCall

--- springboot-service-item/src/main/java/ec/learning/springboot/app/item/AppConfig.java
+++ springboot-service-item/src/main/java/ec/learning/springboot/app/item/AppConfig.java
@@ -35,8 +35,10 @@ public class AppConfig {
 							.failureRateThreshold(50)
 							.waitDurationInOpenState(Duration.ofSeconds(10L))
 							.permittedNumberOfCallsInHalfOpenState(5)
+							.slowCallRateThreshold(50)
+							.slowCallDurationThreshold(Duration.ofSeconds(2L))
 							.build())
-					.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(2L)).build())
+					.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3L)).build())
 					.build();
 		});
 	}
