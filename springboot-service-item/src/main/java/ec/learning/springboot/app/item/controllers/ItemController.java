208e146950a1e773168e850244430b11674be78b
Steven <stevengf14@gmail.com>
2022-12-02 00:13:45 -0500

    Profiles item service - RefreshScoope - Actuator

--- springboot-service-item/src/main/java/ec/learning/springboot/app/item/controllers/ItemController.java
+++ springboot-service-item/src/main/java/ec/learning/springboot/app/item/controllers/ItemController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,10 +32,14 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
  *
  * @author Steven Guamán - November 2022
  */
+@RefreshScope
 @RestController
 public class ItemController {
 
 	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
@@ -100,8 +106,13 @@ public class ItemController {
 	public ResponseEntity<?> getConfig(@Value("${server.port}") String port) {
 		logger.info(text);
 		Map<String, String> json = new HashMap<>();
-		json.put("text", "text");
+		json.put("text", text);
 		json.put("port", port);
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equalsIgnoreCase("dev")) {
+			json.put("author.name", env.getProperty("configuration.author.name"));
+			json.put("author.email", env.getProperty("configuration.author.email"));
+		}
+
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
