705604a18fa2bd0af6f548e03eb3b7f04c37a69a
Steven <stevengf14@gmail.com>
2022-12-14 13:10:59 -0500

    Many no many relationship

--- springboot-service-users/src/main/java/ec/learning/springboot/app/users/models/entity/User.java
+++ springboot-service-users/src/main/java/ec/learning/springboot/app/users/models/entity/User.java
@@ -1,13 +1,19 @@
 package ec.learning.springboot.app.users.models.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 /**
  *
@@ -35,6 +41,11 @@ public class User implements Serializable {
 
 	@Column(unique = true, length = 100)
 	private String email;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"), uniqueConstraints = {
+			@UniqueConstraint(columnNames = { "user_id", "role_id" }) })
+	private List<Role> roles;
 
 	public Long getId() {
 		return id;
@@ -90,6 +101,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
 	}
 
 	private static final long serialVersionUID = 8913861008657218858L;
