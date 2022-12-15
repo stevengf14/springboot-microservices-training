cbad4b2e205886bfba437da7d6076b47687bed69
Steven <stevengf14@gmail.com>
2022-12-14 22:42:32 -0500

    Configuring json response

---
+++ springboot-service-users/src/main/java/ec/learning/springboot/app/users/RepositoryConfig.java
@@ -0,0 +1,23 @@
+package ec.learning.springboot.app.users;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import ec.learning.springboot.app.users.models.entity.Role;
+import ec.learning.springboot.app.users.models.entity.User;
+
+/**
+ *
+ * @author Steven Guamán - December 2022
+ */
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(User.class, Role.class);
+	}
+
+}
