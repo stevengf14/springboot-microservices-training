9f659267992bbac8a98a81345f1941bc7500665b
Steven <stevengf14@gmail.com>
2023-01-03 22:40:43 -0500

    success and error events management

---
+++ springboot-service-oauth/src/main/java/ec/learning/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
@@ -0,0 +1,31 @@
+package ec.learning.springboot.app.oauth.security.event;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationSuccessErrorHandler implements AuthenticationEventPublisher {
+
+	private Logger log = LoggerFactory.getLogger(AuthenticationSuccessErrorHandler.class);
+
+	@Override
+	public void publishAuthenticationSuccess(Authentication authentication) {
+		UserDetails user = (UserDetails) authentication.getPrincipal();
+		String message = "Login Success: " + user.getUsername();
+		System.out.println(message);
+		log.info(message);
+	}
+
+	@Override
+	public void publishAuthenticationFailure(AuthenticationException exception, Authentication authentication) {
+		String message = "Login Error: " + exception.getMessage();
+		System.out.println(message);
+		log.error(message);
+	}
+
+}
