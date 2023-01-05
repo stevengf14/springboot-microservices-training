1a73183b3a0df296d3a1c8f9d1d1b10c9c9b086f
Steven <stevengf14@gmail.com>
2023-01-04 23:36:54 -0500

    Attempts p2

--- springboot-service-oauth/src/main/java/ec/learning/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
+++ springboot-service-oauth/src/main/java/ec/learning/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
@@ -2,6 +2,7 @@ package ec.learning.springboot.app.oauth.security.event;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -9,10 +10,17 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import ec.learning.springboot.app.commons.users.models.entity.User;
+import ec.learning.springboot.app.oauth.services.IUserService;
+import feign.FeignException;
+
 @Component
 public class AuthenticationSuccessErrorHandler implements AuthenticationEventPublisher {
 
 	private Logger log = LoggerFactory.getLogger(AuthenticationSuccessErrorHandler.class);
+
+	@Autowired
+	private IUserService userService;
 
 	@Override
 	public void publishAuthenticationSuccess(Authentication authentication) {
@@ -21,10 +29,16 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 			return;
 		}
 
-		UserDetails user = (UserDetails) authentication.getPrincipal();
-		String message = "Login Success: " + user.getUsername();
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		String message = "Login Success: " + userDetails.getUsername();
 		System.out.println(message);
 		log.info(message);
+
+		User user = userService.findByUsername(authentication.getName());
+		if (user.getAttempts() != null && user.getAttempts() > 0) {
+			user.setAttempts(0);
+			userService.update(user, user.getId());
+		}
 	}
 
 	@Override
@@ -32,6 +46,25 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 		String message = "Login Error: " + exception.getMessage();
 		System.out.println(message);
 		log.error(message);
+
+		try {
+			User user = userService.findByUsername(authentication.getName());
+			if (user.getAttempts() == null) {
+				user.setAttempts(0);
+			}
+			log.info("current number of attempts: " + user.getAttempts());
+			user.setAttempts(user.getAttempts() + 1);
+			log.info("Number of attempts after update: " + user.getAttempts());
+			if (user.getAttempts() >= 3) {
+				log.error(String.format("The user %s has been disabled for exceeding the number of attempts (3)",
+						user.getUsername()));
+				user.setEnabled(false);
+			}
+			userService.update(user, user.getId());
+		} catch (FeignException e) {
+			log.error(String.format("The user %s doesn't exists", authentication.getName()));
+		}
+
 	}
 
 }
