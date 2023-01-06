6c4e6033535fa0e5ba45115ae9b828b10ac9d05d
Steven <stevengf14@gmail.com>
2023-01-05 23:39:22 -0500

    SecurityConfig class

---
+++ springboot-service-gateway-server/src/main/java/ec/learning/springboot/app/gateway/security/SpringSecurityConfig.java
@@ -0,0 +1,15 @@
+package ec.learning.springboot.app.gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+	@Bean
+	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+		return http.authorizeExchange().anyExchange().authenticated().and().csrf().disable().build();
+	}
+}
