a98606c37e94e2c1031991d5560004981196d52d
Steven <stevengf14@gmail.com>
2023-01-06 00:06:33 -0500

    security routes rules

--- springboot-service-gateway-server/src/main/java/ec/learning/springboot/app/gateway/security/SpringSecurityConfig.java
+++ springboot-service-gateway-server/src/main/java/ec/learning/springboot/app/gateway/security/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package ec.learning.springboot.app.gateway.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -10,6 +11,12 @@ public class SpringSecurityConfig {
 
 	@Bean
 	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
-		return http.authorizeExchange().anyExchange().authenticated().and().csrf().disable().build();
+		return http.authorizeExchange().pathMatchers("/api/security/oauth/**").permitAll()
+				.pathMatchers(HttpMethod.GET, "/api/products/getAll", "/api/items/getAll", "/api/users/users",
+						"/api/products/get/{id}", "/api/items/get/{id}/quantity/{quantity}")
+				.permitAll().pathMatchers(HttpMethod.GET, "/api/users/users/{id}", "/api/users/users/search/**")
+				.hasAnyRole("ADMIN", "PRINCESS")
+				.pathMatchers("/api/products/**", "/api/items/**", "/api/users/users/**").hasRole("ADMIN").anyExchange()
+				.authenticated().and().csrf().disable().build();
 	}
 }
